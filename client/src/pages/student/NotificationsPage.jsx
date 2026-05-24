3aa69694ada8257b1a48b97d76571a7a2f17c16b
taniakhanam11 <106593532+taniakhanam11@users.noreply.github.com>
2026-05-24 22:51:38 +0600

    Notification pages

--- client/src/pages/student/NotificationsPage.jsx
+++ client/src/pages/student/NotificationsPage.jsx
@@ -2,7 +2,37 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const NotificationsPage = () => {
-  return <></>;
+  return <>
+  
+  <div className="space-y-6">
+    </div>
+    <div className="card">
+   {/* Card Header */}
+          <div className="card-header">
+            <div className="flex items-center justify-between">
+              <div>
+                <h1 className="card-title text-2xl font-bold text-slate-800 mb-2">
+                  Notifications
+                </h1>
+                <p className="card-subtitle text-[#17a2b8]">
+                  Stay updated with your project progress and deadlines
+                </p>
+              </div>
+     
+            </div>
+          </div>
+
+
+        {/* Notification Stats */}
+         <div className="gird gird-cols-1 md:gird-cols-4
+        gap-4 mb-6">
+
+        </div>
+
+    </div>
+    
+    
+    </>;
 };
 
 export default NotificationsPage;
