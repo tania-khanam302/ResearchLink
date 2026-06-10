6e1a4c7aa5ec5a1883eaaf943679a3016f19c3c7
taniakhanam11 <106593532+taniakhanam11@users.noreply.github.com>
2026-06-10 21:51:59 +0600

    admin dashboard

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -18,7 +18,16 @@ import { Plus, View } from "lucide-react";
 const AdminDashboard = () => {
   return (
     <>
-    
+      <div className="space-y-6">
+        {/* header */}
+        <div className="bg-gradient-to-r from-[#17a2b8] to-purple-500 rounded-lg text-white p-4">
+          <h1 className="text-2xl font-bold mb-2">Admin Dashboard</h1>
+          <p className="text-blue-100">
+            Manage the entire project management system and oversee all
+            activities.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
