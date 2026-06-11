0162c8cbcd5a824caef9a30fb5d6efee9a0a427c
taniakhanam11 <106593532+taniakhanam11@users.noreply.github.com>
2026-06-11 15:04:54 +0600

    ManageStudent page

--- client/src/pages/admin/ManageStudents.jsx
+++ client/src/pages/admin/ManageStudents.jsx
@@ -106,7 +106,7 @@ const ManageStudents = () => {
 
     if (editingStudent) {
       dispatch(updateStudent({ id: editingStudent._id, data: formData }));
-    }   
+    }
     handleCloseModal();
   };
 
@@ -140,7 +140,388 @@ const ManageStudents = () => {
 
   return (
     <>
+      <div className="space-y-6 bg-[url('/bg.jpg')] bg-auto bg-repeat bg-fixed">
+        {/*header */}
+        <div className="card shadow-lg rounded-md">
+          <div className="card-header flex flex-col md:flex-row justify-between items-start md:items-center">
+            <div>
+              <h1 className="card-title text-2xl font-bold text-slate-800 mb-2">
+                Manage Students
+              </h1>
+              <p className="card-subtitle text-[#17a2b8]">
+                Add, edit, and manage student accounts
+              </p>
+            </div>
+            <button className="btn-primary bg-[#17a2b8] hover:bg-[#138496] text-white px-4 font-medium h-10 rounded-md flex items-center space-x-2 mt-4 md:mt-0">
+              <span>Add New Student</span>
+            </button>
+          </div>
+        </div>
+        {/* Add New Student */}
+        {/* <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white rounded-sm w-full max-w-md mx-4 max-h-[90vh] overflow-y-auto">
+            <div className="card-header rounded-t-lg py-4 p-3 mb-0 bg-blue-50 flex justify-between items-center sticky top-0 z-10">
+              <h3 className="text-lg font-semibold text-slate-900">
+                Add Student
+              </h3>
+              <button className="text-slate-400 hover:text-slate-600">
+                <X className="w-6 h-6 text-[#17a2b8]" />
+              </button>
+            </div>
 
+            <div className="p-6 mb-2">
+              <form className="space-y-4">
+                <div>
+                  <label className="block text-sm font-medium text-slate-700 mb-1">
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    required
+                    className="input-field w-full p-2 border-b border-slate-400 focus:outline-none"
+                    placeholder="Enter student full name"
+                  />
+                </div>
+
+                <div>
+                  <label className="block text-sm font-medium text-slate-700 mb-1">
+                    E-mail
+                  </label>
+                  <input
+                    type="email"
+                    required
+                    className="input-field w-full p-2 border-b border-slate-400 focus:outline-none"
+                    placeholder="Enter student email"
+                  />
+                </div>
+
+                <div>
+                  <label className="block text-sm font-medium text-slate-700 mb-1">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    required
+                    className="input-field w-full p-2 border-b border-[#17a2b8] focus:outline-none focus:ring-1 focus:ring-[#17a2b8] pr-10"
+                    placeholder="Enter password"
+                  />
+                </div>
+
+                <div>
+                  <label className="block text-sm font-medium text-slate-700 mb-1">
+                    Department
+                  </label>
+
+                  <select
+                    className="input-field w-full p-2 border-b border-slate-400 focus:outline-none"
+                    required
+                  >
+                    <option value="" disabled>
+                      Select Department
+                    </option>
+                    <option value="Computer Science">Computer Science</option>
+                    <option value="Software Engineering">
+                      Software Engineering
+                    </option>
+                    <option value="Information Technology">
+                      Information Technology
+                    </option>
+                    <option value="Data Science">Data Science</option>
+                    <option value="Electrical Engineering">
+                      Electrical Engineering
+                    </option>
+                    <option value="Mechanical Engineering">
+                      Mechanical Engineering
+                    </option>
+                    <option value="Civil Engineering">Civil Engineering</option>
+                    <option value="Business Administration">
+                      Business Administration
+                    </option>
+                  </select>
+                </div>
+
+                <div className="flex justify-end space-x-3 pt-4">
+                  <button
+                    type="button"
+                    className="btn-danger text-white px-4 font-medium h-11 rounded-md flex items-center space-x-2 mt-4 md:mt-0 shadow-md"
+                  >
+                    Cancel
+                  </button>
+
+                  <button
+                    type="submit"
+                    className="bg-[#138496] hover:bg-[#17a2b8] text-white px-4 font-medium h-11 rounded-md flex items-center space-x-2 mt-4 md:mt-0 shadow-md"
+                  >
+                    Add Student
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div> */}
+
+        {/* Stats cards */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="card shadow-lg rounded-md">
+            <div className="flex items-center">
+              <div className="p-3 bg-blue-100 rounded-lg">
+                <Users className="w-6 h-6 text-[#17a2b8]" />
+              </div>
+              <div className="ml-4">
+                <p className="text-sm font-medium text-slate-600">
+                  Total Students
+                </p>
+                <p className="text-lg font-semibold text-slate-800"></p>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-lg rounded-md">
+            <div className="flex items-center">
+              <div className="p-3 bg-blue-100 rounded-lg">
+                <CheckCircle className="w-6 h-6 text-purple-600" />
+              </div>
+
+              <div className="ml-4">
+                <p className="text-sm font-medium text-slate-600">
+                  Completed Projects
+                </p>
+                <p className="text-lg font-semibold text-slate-800"></p>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-lg rounded-md">
+            <div className="flex items-center">
+              <div className="p-3 bg-blue-100 rounded-lg">
+                <TriangleAlert className="w-6 h-6 text-yellow-600" />
+              </div>
+
+              <div className="ml-4">
+                <p className="text-sm font-medium text-slate-600">Unassigned</p>
+                <p className="text-lg font-semibold text-slate-800"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Search Students */}
+        <div className="card bg-white rounded-md shadow-[0_0.5rem_2rem_rgba(0,0,0,0.15)] flex flex-col md:flex-row gap-4">
+          <div className="flex-1">
+            <label className="block mb-2 card-title text-md font-semibold text-[#17a2b8]">
+              Search Students
+            </label>
+
+            <input
+              type="text"
+              placeholder="Search by name or email..."
+              className="input-field outline-none p-1 border border-slate-300 w-[250px] "
+            />
+          </div>
+
+          <div className="w-full md:w-48">
+            <label className="block mb-2 text-md font-semibold text-[#17a2b8]">
+              Filter Department
+            </label>
+            <select
+              className="input-field w-full outline-none p-1 border border-slate-300"
+              value={filterDepartment}
+            >
+              <option value="all">All Departments</option>
+            </select>
+          </div>
+        </div>
+
+        {/* Students table */}
+        <div className="card bg-white rounded-md shadow-[0_0.5rem_2rem_rgba(0,0,0,0.15)] overflow-hidden">
+          <div className="card-header">
+            <h2 className="card-title text-lg font-semibold text-[#17a2b8]">
+              Students List
+            </h2>
+          </div>
+          <div className="overflow-x-auto">
+            <table className="w-full text-left border-collapse overflow-x-hidden">
+              <thead className="bg-slate-100">
+                <tr className=" text-[#138496] text-xs font-semibold uppercase">
+                  <th className="px-6 py-3 text-left tracking-wide">
+                    Student Info
+                  </th>
+                  <th className="px-6 py-5 text-left ">Email</th>
+                  <th className="px-6 py-5 text-left ">Department & Year</th>
+                  <th className="px-6 py-5 text-left ">Supervisor</th>
+                  <th className="px-6 py-5 text-left ">Project Title</th>
+                  <th className="px-6 py-5 text-left">Action</th>
+                </tr>
+              </thead>
+
+              <tbody className="bg-white divide-y divide-slate-200">
+                <tr className="hover:bg-slate-50">
+                  <td className="px-6 py-4">
+                    <div>
+                      <div className="text-sm font-medium text-slate-900"></div>
+                      <div className="text-sm font-medium text-slate-900"></div>
+                    </div>
+                  </td>
+                  {/* extra */}
+                  <td className="text-sm font-medium text-slate-900"></td>
+                  <td className="px-6 py-4 whitespace-nowrapp">
+                    <div className="text-sm text-slate-900"></div>
+
+                    <div className="text-sm text-slate-500"></div>
+                  </td>
+
+                  <td className="px-6 py-4 whitespace-nowrapp"></td>
+
+                  <td className="px-6 py-4">
+                    <div className="text-sm text-slate-900"></div>
+                  </td>
+
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                    <button className="text-[#17a2b8] hover:text-blue-900 pe-2">
+                      Edit
+                    </button>
+                    <button className="text-red-600 hover:text-red-900">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            {/*   No students found matching your criteria. */}
+            <div className="text-center py-8 text-slate-500">
+              No students found matching your criteria.
+            </div>
+          </div>
+          {/* edit student model */}
+          {/* <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+              <div className="bg-white rounded-sm w-full max-w-md mx-4">
+                <div className="card-header rounded-t-lg py-4 p-3 mb-0 bg-blue-50 flex justify-between items-center">
+                  <h3 className="text-lg font-semibold text-slate-900 ">
+                    Edit Student
+                  </h3>
+                  <button
+                    className="text-slate-400 hover:to-slate-600"
+                  >
+                    <X className="w-6 h-6 text-[#17a2b8]" />
+                  </button>
+                </div>
+
+                <div className="p-6  mb-2">
+                  <form className="space-y-4">
+                    <div>
+                      <label className="block text-sm font-medium text-slate-700 mb-1">
+                        Full Name
+                      </label>
+                      <input
+                        type="text"
+                        required
+                        className="input-feild w-full p-2 border-b border-slate-400 focus:outline-none"
+                      />
+                    </div>
+
+                    <div>
+                      <label className="block text-sm font-medium text-slate-700 mb-1">
+                        E-mail
+                      </label>
+                      <input
+                        type="email"
+                        required
+                        className="input-feild w-full p-2 border-b border-slate-400 focus:outline-none"
+                      />
+                    </div>
+
+                    <div>
+                      <label className="block text-sm font-medium text-slate-700 mb-1">
+                        Department
+                      </label>
+
+                      <select
+                        className="input-feild w-full p-2 border-b border-slate-400 focus:outline-none"
+                        required
+                      >
+                        <option value="Select Department">
+                          Select Department
+                        </option>
+                        <option value="Computer Science">
+                          Computer Science
+                        </option>
+                        <option value="Software Engineering">
+                          Software Engineering
+                        </option>
+                        <option value="Information Technology">
+                          Information Technology
+                        </option>
+                        <option value="Data Science">Data Science</option>
+                        <option value="Electrical Engineering">
+                          Electrical Engineering
+                        </option>
+                        <option value="Mechanical Engineering">
+                          Mechanical Engineering
+                        </option>
+                        <option value="Civil Engineering">
+                          Civil Engineering
+                        </option>
+                        <option value="Business Administration">
+                          Business Administration
+                        </option>
+                      </select>
+                    </div>
+
+                    <div className="flex justify-end space-x-3 pt-4">
+                      <button
+                        type="button"
+                        className="btn-danger text-white px-4 font-medium h-11 rounded-md flex items-center space-x-2 mt-4 md:mt-0 shadow-md"
+                      >
+                        Cancel
+                      </button>
+
+                      <button
+                        type="submit"
+                        className="btn-secondary bg-[#138496] hover:bg-[#17a2b8] text-white px-4 font-medium h-11 rounded-md flex items-center space-x-2 mt-4 md:mt-0 shadow-md"
+                      >
+                        Update Student
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div> */}
+
+          {/* delete student */}
+          {/* <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+              <div className="card bg-white rounded-sm w-full max-w-md mx-4 shadow-xl">
+                <div className="grid items-center mb-4">
+                  <div className="flex-shrink-0 w-10 h-10 mx-auto flex items-center justify-center rounded-full bg-red-100">
+                    <AlertTriangle className=" w-6 h-6 text-red-600" />
+                  </div>
+
+                  <div className="text-center ">
+                    <h3 className="text-lg font-medium text-slate-900 mb-2">
+                      Delete Student
+                    </h3>
+                    <p className="text-sm text-slate-500 mb-4">
+                      Are you sure you want to delete this student
+                      <span>
+                        Jhon? This action cannot be undone.
+                      </span>
+                    </p>
+
+                    <div className="flex justify-center space-x-3">
+                      <button className="btn-secondary text-white px-4 font-medium h-11 rounded-md flex items-center space-x-2 mt-4 md:mt-0 shadow-md">
+                        Cancel
+                      </button>
+                      <button
+                        className="py-2 bg-red-500 text-white hover:bg-red-600 px-4 font-medium h-11 rounded-md flex items-center space-x-2 mt-4 md:mt-0 shadow-md"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div> */}
+        </div>
+      </div>
     </>
   );
 };
