0e956c8aff3153080c053c7c979bdf5c706fbb2f
tania-khanam302 <taniakhanam023@gmail.com>
2026-08-06 14:23:58 +0600

    Update NotFound page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -9,9 +9,13 @@ const NotFound = () => {
   }, [location.pathname]);
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 bg-[url('/bg.jpg')] bg-auto bg-repeat bg-fixed">
       <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold">404</h1>
+        
+        <h1 className="text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-[#138496]">
+          404
+        </h1>
+
         <p className="mb-4 text-xl text-gray-600">Oops! Page not found</p>
         <a href="/" className="text-blue-500 underline hover:text-blue-700">
           Return to Home
