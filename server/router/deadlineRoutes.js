ca203892670175810677feed84d5b601e8003b32
taniakhanam11 <106593532+taniakhanam11@users.noreply.github.com>
2026-08-09 18:37:49 +0600

    deadline routes

---
+++ server/router/deadlineRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { createDeadline } from "../controllers/deadlineController.js";
+import {
+  isAuthenticated,
+  isAuthorized,
+} from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.post(
+  "/create-deadline/:id",
+  isAuthenticated,
+  isAuthorized("Admin-Teacher" , "Co-Admin"),
+  // isAuthorized("Admin-Teacher"),
+  createDeadline,
+);
+
+export default router;
