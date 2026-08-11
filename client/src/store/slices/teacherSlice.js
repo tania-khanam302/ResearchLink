2276ac12e15795699c72f68463cc3e5f64ec321c
taniakhanam11 <106593532+taniakhanam11@users.noreply.github.com>
2026-08-11 14:53:55 +0600

    getTeacherDashboardStats

--- client/src/store/slices/teacherSlice.js
+++ client/src/store/slices/teacherSlice.js
@@ -2,6 +2,22 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
 
+
+export const getTeacherDashboardStats = createAsyncThunk(
+    "getTeacherDashboardStats",
+    async (data, thunkAPI) => {
+        try {
+            const res = await axoiosInstance.get("/teacher/fetch-dashboard-stats");
+            return res.data.data?.dashboardStats || res.data.data;
+        } catch (error) {
+            toast.error
+            (error.response.data.message || "Failed to fetch dashboard stats"
+            );
+            return thunkAPI.rejectWithValue(error.response.data.message);
+        }
+    }
+)
+
 const teacherSlice = createSlice({
   name: "teacher",
   initialState: {
