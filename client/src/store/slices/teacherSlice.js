bf47a2e8202d44b02caf1e49d99ca8ca4214b7b4
taniakhanam11 <106593532+taniakhanam11@users.noreply.github.com>
2026-08-12 09:35:00 +0600

    getTeacherDashboardStats

--- client/src/store/slices/teacherSlice.js
+++ client/src/store/slices/teacherSlice.js
@@ -1,25 +1,58 @@
+
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
 
+// get teacher dashboard stats
+// export const getTeacherDashboardStats = createAsyncThunk(
+//   "teacher/getTeacherDashboardStats",
+//   async (_, thunkAPI) => {
+//     try {
+//       const res = await axiosInstance.get(
+//         "/teacher/fetch-dashboard-stats"
+//       );
+//       return res.data.data?.dashboardStats || res.data.data;
+//     } catch (error) {
+//       toast.error(error.response.data.message || "Failed tp fetch dashboard stats")
+//       return thunkAPI.rejectWithValue(error.response?.data.message );
+//       // const message =
+//       //   error.response?.data?.message ||
+//       //   "Failed to fetch dashboard stats";
 
+//       // toast.error(message);
+
+//       // return thunkAPI.rejectWithValue(error.response?.data.message );
+//     }
+//   }
+// );
+
+
+
+// Get teacher dashboard stats
 export const getTeacherDashboardStats = createAsyncThunk(
-    "getTeacherDashboardStats",
-    async (data, thunkAPI) => {
-        try {
-            const res = await axoiosInstance.get("/teacher/fetch-dashboard-stats");
-            return res.data.data?.dashboardStats || res.data.data;
-        } catch (error) {
-            toast.error
-            (error.response.data.message || "Failed to fetch dashboard stats"
-            );
-            return thunkAPI.rejectWithValue(error.response.data.message);
-        }
+  "teacher/getTeacherDashboardStats",
+  async (_, thunkAPI) => {
+    try {
+      const res = await axiosInstance.get(
+        "/teacher/fetch-dashboard-stats"
+      );
+
+      return res.data.data.dashboardStats;
+    } catch (error) {
+      const message =
+        error.response?.data?.message ||
+        "Failed to fetch dashboard stats";
+
+      toast.error(message);
+
+      return thunkAPI.rejectWithValue(message);
     }
-)
+  }
+);
 
 const teacherSlice = createSlice({
   name: "teacher",
+
   initialState: {
     assignedStudents: [],
     files: [],
@@ -28,8 +61,26 @@ const teacherSlice = createSlice({
     loading: false,
     error: null,
   },
+
   reducers: {},
-  extraReducers: (builder) => {},
+
+  extraReducers: (builder) => {
+    builder
+      // .addCase(getTeacherDashboardStats.pending, (state) => {
+      //   state.loading = true;
+      //   state.error = null;
+      // })
+
+      .addCase(getTeacherDashboardStats.fulfilled, (state, action) => {
+        state.loading = false;
+        state.dashboardStats = action.payload;
+      })
+
+      // .addCase(getTeacherDashboardStats.rejected, (state, action) => {
+      //   state.loading = false;
+      //   state.error = action.payload;
+      // });
+  },
 });
 
 export default teacherSlice.reducer;
