629529b97e5eaabe0121982512dc073fc91ce4ff
taniakhanam11 <106593532+taniakhanam11@users.noreply.github.com>
2026-08-13 11:05:37 +0600

    assign supervigor moduft

--- client/src/pages/admin/AssignSupervisor.jsx
+++ client/src/pages/admin/AssignSupervisor.jsx
@@ -5,11 +5,10 @@ import {
   assignSupervisor as assignSupervisorThunk,
   getAllUsers,
 } from "../../store/slices/adminSlice";
-import { AlertTriangle, CheckCircle, Search, Users } from "lucide-react";
+import { AlertTriangle, CheckCircle, Users } from "lucide-react";
 
 const AssignSupervisor = () => {
   const dispatch = useDispatch();
-
   const [searchTerm, setSearchTerm] = useState("");
   const [filterStatus, setFilterStatus] = useState("all");
   const [selectedSupervisor, setSelectedSupervisor] = useState({});
@@ -47,7 +46,8 @@ const AssignSupervisor = () => {
         status: p.status,
         supervisor: p.supervisor?.name || null,
         supervisorId: p.supervisor?._id || null,
-        studentId: p.student?.name || "unknown",
+        // studentId: p.student?.name || "unknown",
+        studentId: p.student?._id,
         studentName: p.student?.name || "-",
         studentEmail: p.student?.email || "-",
         deadline: p.deadline
@@ -66,14 +66,22 @@ const AssignSupervisor = () => {
       (row.title || "").toLowerCase().includes(searchTerm.toLowerCase());
 
     const status = row.supervisorId ? "assigned" : "unassigned";
-    const matchesStatus = filterStatus === "all" || status === filterStatus;
-    return matchesSearch;
+    const matchesFilter = filterStatus === "all" || status === filterStatus;
+    return matchesSearch && matchesFilter;
+    // const matchesStatus = filterStatus === "all" || status === filterStatus;
+    // return matchesSearch && matchesStatus;
   });
 
   const [pendingFor, setPendingFor] = useState(null);
 
-  const handleAssignSupervisor = (projectId, supervisorId) => {
-    setSelectSupervisor((prev) => ({
+  // const handleAssignSupervisor = (projectId, supervisorId) => {
+  //   setSelectedSupervisor((prev) => ({
+  //     ...prev,
+  //     [projectId]: supervisorId,
+  //   }));
+  // };
+  const handleSupervisorSelect = (projectId, supervisorId) => {
+    setSelectedSupervisor((prev) => ({
       ...prev,
       [projectId]: supervisorId,
     }));
@@ -139,7 +147,7 @@ const AssignSupervisor = () => {
     },
   ];
 
-  // TABLE HEADER
+  // table header
   const headers = [
     "Student",
     "Project Title",
@@ -151,12 +159,13 @@ const AssignSupervisor = () => {
   ];
 
   const Badge = ({ color, children }) => {
-    return(
-      <span>
+    return (
+      <span
         className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${color}`}
-         {children}
+      >
+        {children}
       </span>
-    )
+    );
   };
 
   return (
@@ -171,8 +180,8 @@ const AssignSupervisor = () => {
             </p>
           </div>
         </div>
-   
-{/* Search Students */}
+
+        {/* Search Students */}
         <div className="card bg-white rounded-md shadow-[0_0.5rem_2rem_rgba(0,0,0,0.15)] flex flex-col md:flex-row gap-4">
           <div className="flex-1">
             <label className="block mb-2 card-title text-md font-semibold text-[#17a2b8]">
@@ -183,106 +192,208 @@ const AssignSupervisor = () => {
               type="text"
               placeholder="Search by student name or project title..."
               className="input-field outline-none p-1 border border-slate-300 w-[350px] "
-                value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
           </div>
 
           <div className="w-full md:w-48">
             <label className="block mb-2 text-md font-semibold text-[#17a2b8]">
-             Filter Status
+              Filter Status
             </label>
             <select
               className="input-field w-full outline-none p-1 border border-slate-300"
               value={filterStatus}
-           onChange={(e) => setFilterStatus(e.target.value)}
+              onChange={(e) => setFilterStatus(e.target.value)}
             >
-
-             <option value="all">All Students</option>    
-        <option value="assigned">Assigned</option>  
-        <option value="unassigned">Unassigned</option>
+              <option value="all">All Students</option>
+              <option value="assigned">Assigned</option>
+              <option value="unassigned">Unassigned</option>
             </select>
           </div>
         </div>
 
+        {/* student assignments table */}
+        <div className="card bg-white rounded-md shadow-[0_0.5rem_2rem_rgba(0,0,0,0.15)] overflow-hidden">
+          <div className="card-header">
+            <h2 className="card-title text-lg font-semibold text-[#17a2b8]">
+              Student Assignments
+            </h2>
+          </div>
 
-
-    {/* student assignments table */}
-    <div className="card bg-white rounded-md shadow-[0_0.5rem_2rem_rgba(0,0,0,0.15)] overflow-hidden">
-        <div className="card-header">
-            <h2 className="card-title text-lg font-semibold text-[#17a2b8]">Student Assignments</h2>
-            </div>
-
-            
           <div className="overflow-x-auto">
             <table className="w-full">
-              <thead className="bg-slate-50">
-                 <tr>
-                  {headers.map((h)=>{
+              <thead className="bg-slate-50 text-left px-4">
+                <tr>
+                  {headers.map((h) => {
                     return (
                       <th
-                      key={h}
-                      className="px-6 py-3 text-[#138496] text-xs font-semibold uppercase"
+                        key={h}
+                        className="px-2 py-6 text-[#138496] text-xs font-semibold uppercase"
                       >
                         {h}
                       </th>
                     );
                   })}
-                 </tr>
+                </tr>
               </thead>
               <tbody className="bg-white divide-y divide-slate-200">
-                {filtered.map(row=>(
-                  <tr key={row.projectId} 
-                  
-                  className="hover:bg-slate-50">
-                    <td className="px-6 py-4 whitespace-nowrap">
-                        <div>
-                            <div className="text-sm font-medium text-slate-900">
-                                {row.studentName}
-                            </div>
-                            <div className="text-sm text-slate-500">
-                                {row.studentEmail}
-                            </div>
+                {filtered.map((row) => (
+                  <tr key={row.projectId} className="hover:bg-slate-50">
+                    {/* Student */}
+                    <td className="px-2 py-4 whitespace-nowrap">
+                      <div>
+                        <div className="text-sm font-medium text-slate-900">
+                          {row.studentName}
                         </div>
+                        <div className="text-sm text-slate-500">
+                          {row.studentEmail}
+                        </div>
+                      </div>
                     </td>
-                    <td className="px-6 py-4">{row.title}</td>
 
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <td className="px-6 py-4  whitespace-nowrap">
-                        {row.supervisor !== null ? (
-                          <Badge 
-                          color={"bg-green-100 text-green-800"} children={row.supervisor}
+                    {/* Project Title */}
+                    <td className="px-2 py-4">{row.title}</td>
+
+                    {/* Supervisor */}
+                    <td className="px-2 py-4 whitespace-nowrap">
+                      <div className=" whitespace-nowrap">
+                        {row.supervisor ? (
+                          <Badge
+                            color={"bg-green-100 text-green-800"}
+                            children={row.supervisor}
                           />
-                          
-                          ): (
-                          <Badge 
-                          color={"bg-red-100 text-red-800"}
-                          children={
-                            row.status === "rejecte"
-                            ? "Rejected"
-                            : "Not Assigned"
-                          }
+                        ) : (
+                          <Badge
+                            color={"bg-red-100 text-red-800"}
+                            children={
+                              row.status === "rejected"
+                                ? "Rejected"
+                                : "Not Assigned"
+                            }
                           />
-                          )
+                        )}
+                      </div>
+                    </td>
+
+                    {/* deadline */}
+                    <td className="px-2 py-4 text-[14px]">{row.deadline}</td>
+
+                    {/* update date  */}
+                    <td className="px-2 py-4 text-[14px]">{row.updatedAt}</td>
+
+                    {/* Assign Supervisor */}
+                    <td className="px-2 py-4 whitespace-nowrap">
+                      <select
+                        className="input-field w-full"
+                        value={selectedSupervisor[row.projectId] || ""}
+                        disabled={
+                          !!row.supervisor ||
+                          row.status === "rejected" ||
+                          !row.isApproved
                         }
-                      </td>
+                        onChange={(e) =>
+                          handleAssignSupervisor(row.projectId, e.target.value)
+                        }
+                      >
+                        <option value="" disabled>
+                          Select Supervisor
+                        </option>
+                        {teachers
+                          .filter((t) => t.capacityLeft > 0)
+                          .map((t) => (
+                            <option value={t._id} key={t._id}>
+                              {t.name} ({t.capacityLeft} slots left)
+                            </option>
+                          ))}
+                      </select>
                     </td>
 
-                    <td className="px-6 py-4">{row.deadline}</td>
-                    <td className="px-6 py-4">{row.updatedAt}</td>
+                    {/* Action */}
+                    <td className="px-2 py-4 whitespace-nowrap text-sm font-medium">
+                      {/* <button
+                        className="btn-primary bg-[#17a2b8] hover:bg-[#138496] text-[13px] w-[110px] px-0 pe-0"
+                        onClick={() =>
+                          handleAssign(row.studentId, row.status, row.projectId)
+                        }
+                        disabled={
+                          pendingFor === row.projectId ||
+                          !!row.supervisor ||
+                          row.status === "rejected" ||
+                          !row.isApproved ||
+                          !selectedSupervisor[row.projectId]
+                        }
+                      >
+                        {pendingFor === row.projectId
+                          ? "ASsigning..."
+                          : row.supervisor
+                            ? "Assigned"
+                            : row.status === "rejected"
+                              ? "Rejected"
+                              : !row.isApproved
+                                ? "Not Approved"
+                                : "Assign"}
+                      </button> */}
+                      <button
+                        className="btn-primary bg-[#17a2b8] hover:bg-[#138496] text-[13px] w-[110px] px-0 pe-0"
+                        onClick={() =>
+                          handleAssign(row.studentId, row.status, row.projectId)
+                        }
+                        disabled={
+                          pendingFor === row.projectId ||
+                          !!row.supervisor ||
+                          row.status === "rejected" ||
+                          !row.isApproved ||
+                          !selectedSupervisor[row.projectId]
+                        }
+                      >
+                        {pendingFor === row.projectId
+                          ? "Assigning..."
+                          : row.supervisor
+                            ? "Assigned"
+                            : row.status === "rejected"
+                              ? "Rejected"
+                              : !row.isApproved
+                                ? "Not Approved"
+                                : "Assign"}
+                      </button>
+                    </td>
                   </tr>
-                  )) 
-                }
-                </tbody>
-
+                ))}
+              </tbody>
             </table>
+            {filtered.length === 0 && (
+              <div className="text-center py-8 text-slate-500">
+                No students found matching your criteria
+              </div>
+            )}
           </div>
-    </div>
+        </div>
 
-
-
-
-   </div>
+        {/* summary */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-4">
+          {dashboardCards.map((card, index) => {
+            const Icon = card.icon;
+            return (
+              <div key={index} className="card">
+                <div className="flex items-center">
+                  <div className={`p-3 ${card.bg} rounded-lg`}>
+                    <Icon className={`w-6 h-6 ${card.color}`} />
+                  </div>
+                  <div className="ml-4">
+                    <p className="text-sm font-medium text-slate-500">
+                      {card.title}
+                    </p>
+                    <p className="text-lg font-semibold text-slate-800">
+                      {card.value}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
