d410ef51143235113b28f53200edf87cf84deba9
taniakhanam11 <106593532+taniakhanam11@users.noreply.github.com>
2026-09-08 10:04:48 +0600

    modify project services

--- server/services/projectServices.js
+++ server/services/projectServices.js
@@ -1,34 +1,41 @@
 import ErrorHandler from "../middlewares/error.js";
 import { Project } from "../models/project.js";
 
+// get project by student
 export const getProjectByStudent = async (studentId) => {
   return await Project.findOne({ student: studentId }).sort({ createdAt: -1 });
 };
 
+// create project
 export const createProject = async (projectData) => {
   const project = new Project(projectData);
   await project.save();
   return project;
 };
 
+// get project by id
 export const getProjectById = async (projectId) => {
   const project = await Project.findById(projectId)
     .populate("student", "name email")
     .populate("supervisor", "name email")
     .populate("feedback.supervisorId", "name email");
-
-  if (!project) {
-    throw new ErrorHandler("Project not found", 404);
-  }
-
   return project;
 };
 
+// add files to project
 export const addfilesToProject = async (projectId, files) => {
   const project = await Project.findById(projectId);
+
   if (!project) {
     throw new ErrorHandler("Project not found", 404);
   }
+
+  console.log("UPLOAD PROJECT:", {
+    id: project._id,
+    type: project.type,
+    title: project.title,
+  });
+
   const fileMetaData = files.map((file) => ({
     fileType: file.mimetype,
     fileUrl: file.path,
@@ -38,18 +45,20 @@ export const addfilesToProject = async (projectId, files) => {
 
   project.files.push(...fileMetaData);
   await project.save();
-
   return project;
 };
 
+// get all projects
 export const getAllProjects = async () => {
   const projects = await Project.find()
-    .populate("student", "name email")
+    .populate("student", "name email type")
     .populate("supervisor", "name email")
     .sort({ createdAt: -1 });
-  return projects;
+
+  return projects.filter((project) => project.student?.type === "Project");
 };
 
+// mark complete
 export const markComplete = async (projectId) => {
   const project = await Project.findByIdAndUpdate(
     projectId,
@@ -87,11 +96,10 @@ export const addFeedback = async (
 
   await project.save();
   const latestFeedback = project.feedback[project.feedback.length - 1];
-
   return { project, latestFeedback };
 };
 
-
+// get project by supervisor
 export const getProjectBySupervisor = async (supervisorId) => {
   return await Project.find({ supervisor: supervisorId })
     .populate("student", "name email")
@@ -99,24 +107,19 @@ export const getProjectBySupervisor = async (supervisorId) => {
     .sort({ createdAt: -1 });
 };
 
-// export const getProjectBySupervisor = async (
-// supervisorId
-// ) => {
-//   return await getAllProjects({supervisor:supervisorId});
-// };
-
-export const updateProject = async(id, updatedData)=>{
-  const project = await Project.findByIdAndUpdate(id, updatedData,{
-    new:true,
-    runValidators:true,
+// update project
+export const updateProject = async (id, updatedData) => {
+  const project = await Project.findByIdAndUpdate(id, updatedData, {
+    new: true,
+    runValidators: true,
   })
-  .populate("student", "name email")
-  .populate("supervisor", "name email")
-  if(!project){
+    .populate("student", "name email")
+    .populate("supervisor", "name email");
+  if (!project) {
     throw new ErrorHandler("Project not found", 404);
   }
   return project;
-}
+};
 
 export const deleteProject = async (id) => {
   return await Project.findByIdAndDelete(id);
