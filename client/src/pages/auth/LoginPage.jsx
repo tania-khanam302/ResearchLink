5d0e8e242c77f5d671fb8a27e5e8505e85f9338f
taniakhanam11 <106593532+taniakhanam11@users.noreply.github.com>
2026-09-25 12:59:43 +0600

    modify design

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -89,7 +89,7 @@ const LoginPage = () => {
 
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center px-4">
+      <div className="min-h-screen  bg-[#e4f6fa]  flex items-center justify-center px-4">
         <div className="relative z-10  max-w-md w-full">
           <div className="px-6 py-6 rounded-lg border shadow-[0px_0px_40px_rgba(0,0,0,0.35)]">
             {/* login header */}
